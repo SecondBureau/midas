3ed3137d77fe3e60141bd83dde3967155fd8716f
created by script <romain@2buro-dev>
2013-04-12 18:13:48 +0800

    change

--- vendor/extensions/midas/config/routes.rb
+++ vendor/extensions/midas/config/routes.rb
@@ -2,7 +2,9 @@ Refinery::Core::Engine.routes.append do
 
   # Frontend routes
   namespace :midas do
-    resources :categories, :only => [:index, :show]
+    resources :categories, :only => [:index, :show] do
+      get :show_entries
+    end
     resources :accounts, :only => [:index, :show]
     resources :entries, :only => [:index, :show]
   end
